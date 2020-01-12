3af37dd769dfd54ced61f200fbff6297ff7d079c
antoniomenchon@outlook.com <you@example.com>
2020-01-12 07:36:23 +0100

    priem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './custom.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
+
+
+
+
+  function App() {
+    return (<div className="App">
+      <header className="App-header static-top navbar">
+        <h1 className="Logo">Diseño web Murcia</h1>
       </header>
-    </div>
-  );
-}
+      
+      <div className="flotante1"></div>
+    </div>);
+  }
 
-export default App;
+
+  export default App;
+
